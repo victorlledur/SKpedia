0419a246ea261a42e096967e9fe83511b6d6b88f
Victor Ledur <victorlledur@gmail.com>
2023-11-05 06:41:57 -0300

    feat: google verification

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,16 @@
 import { Request, Response, Router } from "express";
 import assassinoRoutes from "./assassino";
+import express from 'express';
+import path from 'path';
+
+const router = express.Router();
 
 const routes = Router();
+
+router.get('/google_verification.html', (req, res) => {    
+    const arquivoVerificacao = path.join(__dirname, './googleaa16c7ab4b42f18e.html');
+    res.sendFile(arquivoVerificacao);
+  });
 
 routes.get("/", (req:Request, res:Response) =>{
     return res.json("funcionando!!!");
